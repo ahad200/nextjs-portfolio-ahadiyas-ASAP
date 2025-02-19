c60903e6f3e4b20a9a591a1c78e718ec930804dc
Muhammad Ahadiyas Hatumena <79514402+ahad200@users.noreply.github.com>
2025-02-19 18:33:40 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/loremipsum-logo.svg"
+          src="/next.svg"
           alt="loremipsum logo"
           width={180}
           height={38}
