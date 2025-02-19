c6d5a20b10ef1825e39b34ec08a087dfe9493239
Muhammad Ahadiyas Hatumena <79514402+ahad200@users.noreply.github.com>
2025-02-19 17:46:55 +0700

    Delete components directory

--- components/blur-in-provider.tsx
+++
@@ -1,5 +0,0 @@
-import BlurIn from "@/components/ui/blur-in";
-
-export function BlurInDemo() {
-  return <BlurIn word="Blur In" className="text-4xl font-bold text-black dark:text-white" />;
-}
--- components/sparkles-text-provider.tsx
+++
@@ -1,5 +0,0 @@
-import SparklesText from "@/components/ui/sparkles-text";
-
-export function SparklesTextDemo() {
-  return <SparklesText text="Hello World!" className="text-4xl font-medium"></SparklesText>;
-}
